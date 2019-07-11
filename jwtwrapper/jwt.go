1ba15bebbaa2c81cc6fb3db3d84eabfb553c67d3
yushizhao <yushizhao@outlook.com>
2019-07-11 16:17:08 +0800

    jwt

---
+++ jwtwrapper/jwt.go
@@ -0,0 +1,56 @@
+package jwtwrapper
+
+import (
+	"fmt"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func GetMapClaims(tokenStr, secretStr string) (map[string]interface{}, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secretStr), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok {
+		if token.Valid {
+			err := claims.Valid()
+			return claims, err
+		}
+		return nil, fmt.Errorf("Invalid token signature")
+	}
+	return nil, fmt.Errorf("Unexpected claims format: %T", claims)
+}
+
+func IssueTokenStr(claims map[string]interface{}, secretStr string) (string, error) {
+	var claimsMC jwt.MapClaims
+	claimsMC = claims
+	claimsMC["iat"] = time.Now().Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claimsMC)
+
+	// Sign and get the complete encoded token as a string using the secret
+	return token.SignedString([]byte(secretStr))
+}
+
+func IssueTokenStrWithExp(claims map[string]interface{}, secretStr string, second int) (string, error) {
+	var claimsMC jwt.MapClaims
+	claimsMC = claims
+	now := time.Now().Unix()
+	claimsMC["iat"] = now
+	claimsMC["exp"] = now + int64(second)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claimsMC)
+
+	// Sign and get the complete encoded token as a string using the secret
+	return token.SignedString([]byte(secretStr))
+}
--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/yushizhao/authenticator/gawrapper"
+	"github.com/yushizhao/authenticator/jwtwrapper"
 )
 
 func main() {
 	// secret := gawrapper.GenerateSecret()
-	secret := "V6XBUJT5SRL57YIU"
-	name := "lvjidong"
-	issuer := "hubble"
+	// secret := "V6XBUJT5SRL57YIU"
+	// name := "lvjidong"
+	// issuer := "hubble"
 
 	// fmt.Println(secret)
 	// fmt.Println(gawrapper.ComputeTOTP(secret))
-	u := gawrapper.NewOTPAuth(name, secret, issuer)
-	fmt.Println(u)
+	// u := gawrapper.NewOTPAuth(name, secret, issuer)
+	// fmt.Println(u)
 
 	// var err error
 	// err = qrcode.WriteFile(u, qrcode.Medium, 256, "qr.png")
@@ -23,14 +24,36 @@ func main() {
 	// 	fmt.Println(err)
 	// }
 
-	var codeInput string
-	for {
-		fmt.Scanln(&codeInput)
+	// var codeInput string
+	// for {
+	// 	fmt.Scanln(&codeInput)
 
-		ok, err := gawrapper.VerifyTOTP(secret, codeInput)
-		if err != nil {
-			fmt.Println(u)
-		}
-		fmt.Println(ok)
+	// 	ok, err := gawrapper.VerifyTOTP(secret, codeInput)
+	// 	if err != nil {
+	// 		fmt.Println(u)
+	// 	}
+	// 	fmt.Println(ok)
+	// }
+
+	JWTSecret := "xihaijie"
+	c := map[string]interface{}{
+		"foo": "bar",
 	}
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := jwtwrapper.IssueTokenStr(c, JWTSecret)
+	tokenString2, err := jwtwrapper.IssueTokenStrWithExp(c, JWTSecret, 5)
+
+	fmt.Println(tokenString, err)
+
+	cc, err := jwtwrapper.GetMapClaims(tokenString, JWTSecret)
+	fmt.Printf("%v,%v\n", cc, err)
+
+	for {
+		cc2, err := jwtwrapper.GetMapClaims(tokenString2, JWTSecret)
+		fmt.Printf("%v,%v\n", cc2, err)
+
+		time.Sleep(time.Second)
+	}
+
 }
