c114200007749ba5809a214cf192405d37d453d7
yushizhao <yushizhao@outlook.com>
2019-07-12 11:16:04 +0800

    boltwrapper

---
+++ boltwrapper/database.go
@@ -0,0 +1,51 @@
+package boltwrapper
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+type AUTHDB struct {
+	boltptr *bolt.DB
+}
+
+var UserDB AUTHDB
+
+// multiple processes cannot open the same database at the same time
+func init() {
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("user.bolt.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("USER"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+
+	UserDB.boltptr = db
+}
+
+// If the key exists then it will return its byte slice value. If it doesn't exist then it will return nil.
+func (this *AUTHDB) GetUser(name string) (v []byte) {
+	this.boltptr.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("USER"))
+		v = b.Get([]byte(name))
+		return nil
+	})
+	return v
+}
+
+func (this *AUTHDB) SetUser(name string, v []byte) error {
+	return this.boltptr.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("USER"))
+		err := b.Put([]byte("name"), v)
+		return err
+	})
+}
